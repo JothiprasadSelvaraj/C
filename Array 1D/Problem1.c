f7293ad6911ee4dbdb2dc5fe86fb64fa751dc231
jothiprasad <jothiprasad5959@gmail.com>
2024-12-05 19:02:32 +0530

    triangle star pattern

---
+++ Array 1D/Problem1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    int size;
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d", arr[i]);
+    }
+
+    return 0;
+}
